34363ac841269b30c7e38c1f336bcb4ae0b0e938
Tuzki Zhang <rabbitzhang52@gmail.com>
2015-02-03 10:54:32 +0800

    test remote branches

--- git_test.go
+++ git_test.go
@@ -4,13 +4,30 @@ import (
 	"testing"
 )
 
+var path string
+
+func init() {
+	path = "/Users/tuzki/htdocs/go/src/gogit"
+}
+
 func TestBranches(t *testing.T) {
-	git := Git{"/Users/tuzki/htdocs/go/src/gogit"}
+	git := Git{path}
 	branches, err := git.Branches()
 	if err != nil {
 		t.Fatal(err)
 	}
 	if len(branches) < 1 {
 		t.Fatal("there is no branches")
+	}
+}
+
+func TestRemoteBranches(t *testing.T) {
+	git := Git{path}
+	remoteBranches, err := git.RemoteBranches("origin")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(remoteBranches) < 1 {
+		t.Fatal("there is no remote branches")
 	}
 }
